6a4ef4f91afa44db4e07deff89788e181e3abb50
Spencer Saunders <svs1@math.sc.edu>
2020-07-23 22:24:50 -0700

    adding python projects

---
+++ chaos.py
@@ -0,0 +1,12 @@
+#file chaos.py
+#a simple program illustrating chaotic behavior
+
+def main():
+	print("This program illustrates a chaotic function")
+	x= eval(input("Enter a number between 0 and 1: "))
+	for i in range(10):
+		x= 3.9*x*(1-x)
+		print(x)
+
+main();
+input("Press any key to exit");
---
+++ firstpythontest.py
@@ -0,0 +1,8 @@
+import math
+
+x=float(input("Enter 1st number: "))
+y=float(input("Enter 2nd number: "))
+z=float(input("Enter 3rd number: "))
+
+print("the maximum is " + max(x,y,z));
+input("Press any key to close")
